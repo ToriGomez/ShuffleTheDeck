584f044ab323e50f13c33fce838580560137cb86
Victoria Gomez <77286079+ToriGomez@users.noreply.github.com>
2021-02-19 20:22:42 -0700

    Add project files.

---
+++ ShuffleTheDeck/ShuffleTheDeck.vb
@@ -0,0 +1,56 @@
+﻿'Tori Gomez
+'RCET 0265
+'Spring 2021
+'Shuffle the Deck
+'
+
+Option Strict On
+Option Explicit On
+Option Compare Text
+
+Module ShuffleTheDeck
+    Sub Main()
+        Dim userInput As String
+        Dim deck(12, 3) As Boolean
+        Dim value As Integer
+        Dim suit As Integer
+        Dim cont As Boolean = True
+        Console.WriteLine("Press enter for playing card." _
+                           & vbNewLine & "Type shuffle to shuffle all cards" _
+                           & vbNewLine & "Type Q to quit" & vbNewLine)
+        'Repeats/Continues program until told otherwise
+        While cont
+            userInput = Console.ReadLine()
+            Select Case userInput
+                'Shuffles deck when User inputs shuffle
+                Case "Shuffle"
+                    'Shuffle() - Sets all dealt cards to a false value to reset.
+                    libTRG.Shuffle()
+                'Quits program when User inputs Q. Stops repeating loop.
+                Case "Q"
+                    Console.Clear()
+                    Console.WriteLine($"You typed {userInput}, Thank you have a good day.")
+                    Console.Read()
+                    End
+                 'Deals random card when User inputs enter.
+                Case ""
+                    value = CInt(Math.Floor(Rnd() * 12))
+                    suit = CInt(Math.Floor(Rnd() * 3))
+                    Select Case deck(value, suit)
+                        'If card is still available program deals card
+                        Case False
+                            deck(value, suit) = True
+                            'CardArray(,) - determines playing card based on values of a multidimensional array.
+                            libTRG.CardArray(value, suit)
+                        'If card already dealt, Deal() trys another card that has not been dealt.
+                        Case True
+                            libTRG.Deal()
+                    End Select
+                    'Program Writes NOT VALID when User Input is not a functional input.
+                Case Else
+                    Console.WriteLine($"{userInput} is NOT VALID")
+            End Select
+        End While
+        Console.Read()
+    End Sub
+End Module
---
+++ ShuffleTheDeck/libTRG.vb
@@ -0,0 +1,63 @@
+﻿
+Option Explicit On
+Option Strict On
+Option Compare Text
+Module libTRG
+    'If card already dealt, program trys another card that has not been dealt.
+    Sub Deal()
+        Dim deck(12, 3) As Boolean
+        Dim value As Integer
+        Dim suit As Integer
+        value = CInt(Math.Floor(Rnd() * 12))
+        suit = CInt(Math.Floor(Rnd() * 3))
+        Select Case deck(value, suit)
+            Case False
+                deck(value, suit) = True
+                CardArray(value, suit)
+            Case True
+                Deal()
+        End Select
+    End Sub
+
+    'Sets all dealt cards to a false value to reset.
+    Function Shuffle() As Boolean(,)
+        Dim deck(12, 3) As Boolean
+        For i = LBound(deck) To UBound(deck)
+            deck(i, 0) = False
+            deck(i, 1) = False
+            deck(i, 2) = False
+            deck(i, 3) = False
+        Next
+        Return deck
+    End Function
+
+    'Determines playing card based on values of a multidimensional array.
+    Sub CardArray(values As Integer, suits As Integer)
+        Dim suit As String
+        Dim spades As String = "Spades"
+        Dim clubs As String = "Clubs"
+        Dim hearts As String = "Hearts"
+        Dim diamonds As String = "Diamonds"
+        Select Case suits
+            Case 0
+                suit = spades
+            Case 1
+                suit = clubs
+            Case 2
+                suit = hearts
+            Case 3
+                suit = diamonds
+        End Select
+        If values < 9 Then
+            Console.WriteLine($"the {(values + 2)} of {suit}")
+        ElseIf values = 9 Then
+            Console.WriteLine($"the Jack of {suit}")
+        ElseIf values = 10 Then
+            Console.WriteLine($"the Queen of {suit}")
+        ElseIf values = 11 Then
+            Console.WriteLine($"the King of {suit}")
+        ElseIf values = 12 Then
+            Console.WriteLine($"the Ace of {suit}")
+        End If
+    End Sub
+End Module
